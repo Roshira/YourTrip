4532ca428ea832b5614f5529cf790d3b46f51358
Roshira <roshira@ukr.net>
2025-05-03 09:10:01 +0300

    Add interface and realization auth without injection

---
+++ YourTrips.Core/DTOs/Auth/AuthResponseDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YourTrips.Core.DTOs.Auth
+{
+    public class AuthResponseDto
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public string Token { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+    }
+}
---
+++ YourTrips.Infrastructure/Services/JwtTokenGenerator.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YourTrips.Core.Entities;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using YourTrips.Core.Interfaces.Services;
+
+namespace YourTrips.Infrastructure.Services
+{
+    public class JwtTokenGenerator : IJwtTokenGenerator
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtTokenGenerator(IConfiguration cofiguration)
+        {
+            _configuration = cofiguration;
+        }
+
+        public string GenerateToken(User user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString())
+
+            };
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddDays(30),
+                signingCredentials: credentials
+                );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
