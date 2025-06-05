df99fcf7f0018e63dce6734d48f196969b3262fc
Roshira <roshira@ukr.net>
2025-06-05 13:54:08 +0300

    add User sort tests

--- YourTrips.Test/UnitTest1.cs
+++ YourTrips.Test/UnitTest1.cs
@@ -1,16 +1,130 @@
-﻿namespace YourTrips.Test
+﻿using NUnit.Framework;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YourTrips.Core.Entities;
+using YourTrips.Infrastructure.Data;
+using YourTrips.Infrastructure.Services.Admin.Data;
+
+namespace YourTrips.Tests.Services
 {
-    public class Tests
+    [TestFixture]
+    public class UserSortingServiceTests
     {
+        private YourTripsDbContext _context;
+        private UserSortingService _service;
+
         [SetUp]
-        public void Setup()
+        public void SetUp()
         {
+            var options = new DbContextOptionsBuilder<YourTripsDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDb_" + Guid.NewGuid())
+                .Options;
+
+            _context = new YourTripsDbContext(options);
+
+            SeedData(_context);
+            _service = new UserSortingService(_context);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _context.Dispose();
+        }
+
+        private void SeedData(YourTripsDbContext context)
+        {
+            var users = new List<User>
+    {
+        new User
+        {
+            Id = Guid.NewGuid(),
+            UserName = "User1",
+            Email = "u1@test.com",
+            Routes = new List<Route>
+            {
+                new Route { Name = "Route A" },
+                new Route { Name = "Route B" }
+            }
+        },
+        new User
+        {
+            Id = Guid.NewGuid(),
+            UserName = "User2",
+            Email = "u2@test.com",
+            Routes = new List<Route>
+            {
+                new Route { Name = "Route C" }
+            }
+        },
+        new User
+        {
+            Id = Guid.NewGuid(),
+            UserName = "User3",
+            Email = "u3@test.com",
+            Routes = new List<Route>
+            {
+                new Route { Name = "Route D" },
+                new Route { Name = "Route E" },
+                new Route { Name = "Route F" }
+            }
+        },
+        new User
+        {
+            Id = Guid.NewGuid(),
+            UserName = "User4",
+            Email = "u4@test.com",
+            Routes = new List<Route>() // пустий список — ок
+        }
+    };
+
+            context.Users.AddRange(users);
+            context.SaveChanges();
+        }
+
+
+        [Test]
+        public async Task CompareSortingMethods_ShouldReturnSameSortedResult()
+        {
+            var result = await _service.CompareSortingMethods();
+
+            var sequential = result.SequentialResult.UserRoutes;
+            var parallel = result.ParallelResult.UserRoutes;
+
+            Assert.That(sequential.Count, Is.EqualTo(parallel.Count));
+
+            for (int i = 0; i < sequential.Count; i++)
+            {
+                Assert.That(sequential[i].User.Id, Is.EqualTo(parallel[i].User.Id));
+                Assert.That(sequential[i].RoutesCount, Is.EqualTo(parallel[i].RoutesCount));
+            }
         }
 
         [Test]
-        public void Test1()
+        public async Task SequentialSort_ShouldSortByRouteCountDescending()
         {
-            Assert.Pass();
+            var result = await _service.CompareSortingMethods();
+            var sorted = result.SequentialResult.UserRoutes;
+
+            for (int i = 1; i < sorted.Count; i++)
+            {
+                Assert.That(sorted[i - 1].RoutesCount, Is.GreaterThanOrEqualTo(sorted[i].RoutesCount));
+            }
+        }
+
+        [Test]
+        public async Task ParallelSort_ShouldSortByRouteCountDescending()
+        {
+            var result = await _service.CompareSortingMethods();
+            var sorted = result.ParallelResult.UserRoutes;
+
+            for (int i = 1; i < sorted.Count; i++)
+            {
+                Assert.That(sorted[i - 1].RoutesCount, Is.GreaterThanOrEqualTo(sorted[i].RoutesCount));
+            }
         }
     }
 }
